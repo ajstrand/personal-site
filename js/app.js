b6eb19a0016cd8aae5bcd61d2dd23e813c2b92ef
Alex Strand <ajstrand8@gmail.com>
2018-01-23 23:41:07 -0500

    adds project descriptions

--- js/app.js
+++ js/app.js
@@ -1,22 +1,40 @@
 function type() {
-  
-    var cont = $('.main-info .typewriter');
-  
-    var str = cont.text();
-    cont.empty();
+  var cont = $(".main-info .typewriter");
 
-    var arr = str.split('');
-  
-    var i = 0;
-  
-    $.each(arr, function(letter, i){
-      setTimeout(function(){
-        var result = cont.text() + letter;
-        cont.text(result);
-      },100*i);
-    });
-  };
-  
-  $(document).on('ready', function(){
-    type();
+  var str = cont.text();
+  cont.empty();
+
+  var arr = str.split("");
+
+  var i = 0;
+
+  $.each(arr, function(letter, i) {
+    setTimeout(function() {
+      var result = cont.text() + letter;
+      cont.text(result);
+    }, 100 * i);
   });
+}
+
+function toggleDescriptions() {
+  var $project = $(".projects .project");
+  var $desc = $(".desc");
+  $project.on("click", function() {
+    for (let i = 0; i < $desc.length; i++) {
+      const element = $desc[i];
+      $(element).css("display", "none");
+    }
+    var currentDesc = parseInt($(this).attr("data"));
+
+    if ($($desc[currentDesc]).css("display") === "block") {
+      $($desc[currentDesc]).css("display", "none");
+    } else {
+      $($desc[currentDesc]).css("display", "block");
+    }
+  });
+}
+
+$(document).on("ready", function() {
+  type();
+  toggleDescriptions();
+});
--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@ extern crate serde_derive;
 
 use serde_json::value::Value;
 
-
 #[macro_use]
 extern crate askama; // for the Template trait and custom derive macro
 
@@ -21,18 +20,21 @@ use askama::Template; // bring trait in scope
 #[derive(Template)] // this will generate the code...
 // using the template in this path, relative
 // to the templates dir in the crate root
-#[template(path = "template.html")] 
-                                 
+#[template(path = "template.html")]
+
 // the name of the struct can be anything
 // the field name should match the variable name
-// in your template                                 
-struct DataTemplate<'a> { 
+// in your template
+struct DataTemplate<'a> {
     content: &'a str,
     title: &'a str,
     intro_text: &'a str,
-    project_one:&'a str,
-    project_two:&'a str,   
-                   
+    project_one: &'a str,
+    project_two: &'a str,
+    project_three: &'a str,
+    link_one: &'a str,
+    link_two: &'a str,
+    link_three: &'a str,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -40,10 +42,14 @@ struct SiteData<'a> {
     content: &'a str,
     title: &'a str,
     intro_text: &'a str,
-    project_one:&'a str, 
-    project_two:&'a str  
+    project_one: &'a str,
+    project_two: &'a str,
+    project_three: &'a str,
+    link_one: &'a str,
+    link_two: &'a str,
+    link_three: &'a str,
 }
-   
+
 fn main() {
     let mut file = File::open("./data.json").unwrap();
     let mut data = String::new();
@@ -56,34 +62,39 @@ fn main() {
     let intro = json.intro_text;
     let project_one = json.project_one;
     let project_two = json.project_two;
-    let data = DataTemplate { content: about_me, intro_text:intro, 
-    project_one:project_one, project_two:project_two, title: my_title }; // instantiate your struct
+    let project_three = json.project_three;
+    let link_one = json.link_one;
+    let link_two = json.link_two;
+    let link_three = json.link_three;
+
+    let data = DataTemplate {
+        content: about_me,
+        intro_text: intro,
+        project_one: project_one,
+        project_two: project_two,
+        project_three: project_three,
+        link_one: link_one,
+        link_two: link_two,
+        link_three: link_three,
+        title: my_title,
+    }; // instantiate your struct
 
     let path = Path::new("./templates/index.html");
     let display = path.display();
 
     let mut file = match File::create(&path) {
-        Err(why) => panic!("couldn't create {}: {}",
-                           display,
-                           why.description()),
+        Err(why) => panic!("couldn't create {}: {}", display, why.description()),
         Ok(file) => file,
     };
 
-
     match file.write_all(data.render().unwrap().as_bytes()) {
-        Err(why) => {
-            panic!("couldn't write to {}: {}", display,
-                                               why.description())
-        },
+        Err(why) => panic!("couldn't write to {}: {}", display, why.description()),
         Ok(_) => println!("successfully wrote to {}", display),
     }
 
-    
     fs::copy("./templates/index.html", "index.html");
     println!("copied template to an index file in the root");
 
     fs::remove_file("./templates/index.html");
     println!("deleted un needed index file in the template folder");
-
-
 }
