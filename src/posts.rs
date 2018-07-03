f5dc5ba6c6aee75ed9273e9865f32b4f842430cf
Alex Strand <ajstrand8@gmail.com>
2018-07-03 01:25:20 -0400

    update and move code into a module

---
+++ src/posts.rs
@@ -0,0 +1,50 @@
+
+pub mod posts {
+  use std::string::String;
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+use std::fs;
+  pub fn create_posts() {
+    let post_paths = fs::read_dir("./postTemplates").unwrap();
+
+    for path in post_paths {
+        let my_path = &path.unwrap().path();
+        let new_path = Path::new(my_path);
+        create_post(new_path)
+    }
+    println!("added all posts");
+}
+
+use std;
+fn create_post(new_path: &std::path::Path) {
+    use comrak::{markdown_to_html, ComrakOptions};
+
+    let mut file = File::open(new_path).unwrap();
+
+    let mut data = String::new();
+    file.read_to_string(&mut data).unwrap();
+
+    let new_post = markdown_to_html(&data, &ComrakOptions::default());
+
+    let path = Path::new("./templates/my_first_post.html");
+    let display = path.display();
+
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}", display, why.description()),
+        Ok(file) => file,
+    };
+
+    match file.write_all(new_post.as_bytes()) {
+        Err(why) => panic!("couldn't write to {}: {}", display, why.description()),
+        Ok(_) => println!("successfully wrote to {}", display),
+    }
+
+    fs::copy("./templates/my_first_post.html", "my_first_post.html");
+    println!("copied template to a markdown file in the root");
+
+    fs::remove_file("./templates/my_first_post.html");
+    println!("deleted un needed markdown file in the template folder");
+}
+}
