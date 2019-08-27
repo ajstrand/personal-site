95c8979769616e23f8b38f2bdc038fd88b5ebb04
Alex <ajstrand8@gmail.com>
2019-08-27 18:15:30 -0400

    remove gatsby plugin feed that was breaking my production build

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,7 +76,6 @@ module.exports = {
         //trackingId: `ADD YOUR TRACKING ID HERE`,
       },
     },
-    `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
