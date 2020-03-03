b6dad9ce948b7757384251d57f1c798ff666a66c
Alex <ajstrand8@gmail.com>
2020-03-02 22:18:06 -0500

    rip out google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,39 +1,39 @@
 module.exports = {
   siteMetadata: {
-    title:'my blog',
+    title: "my blog",
     author: `Alex Strand`,
     description: `personal blog`,
     siteUrl: `https://github.com/ajstrand/personal-site`,
     social: {
-      twitter: `_alex_strand`,
-    },
+      twitter: `_alex_strand`
+    }
   },
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
+        name: `blog`
+      }
     },
     {
       resolve: `gatsby-mdx`,
       options: {
         // Apply gatsby-mdx to both .mdx and .md files
-        extensions: ['.mdx', '.md'],
-        defaultLayout: require.resolve('./src/templates/blog-post.js'),
+        extensions: [".mdx", ".md"],
+        defaultLayout: require.resolve("./src/templates/blog-post.js"),
         gatsbyRemarkPlugins: [
           {
             resolve: `gatsby-remark-prismjs`,
             options: {
               // Class prefix for <pre> tags containing syntax highlighting;
-            // defaults to 'language-' (eg <pre class="language-js">).
-            // If your site loads Prism into the browser at runtime,
-            // (eg for use with libraries like react-live),
-            // you may use this to prevent Prism from re-processing syntax.
-            // This is an uncommon use-case though;
-            // If you're unsure, it's best to use the default value.
-              classPrefix: 'language-',
+              // defaults to 'language-' (eg <pre class="language-js">).
+              // If your site loads Prism into the browser at runtime,
+              // (eg for use with libraries like react-live),
+              // you may use this to prevent Prism from re-processing syntax.
+              // This is an uncommon use-case though;
+              // If you're unsure, it's best to use the default value.
+              classPrefix: "language-",
               inlineCodeMarker: null
             }
           }
@@ -46,22 +46,22 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `./src/data/`,
-      },
+        path: `./src/data/`
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
+        name: `assets`
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/resume`,
-        name: `resume`,
-      },
+        name: `resume`
+      }
     },
     {
       resolve: `gatsby-transformer-remark`,
@@ -70,28 +70,22 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 590,
-            },
+              maxWidth: 590
+            }
           },
           {
             resolve: `gatsby-remark-responsive-iframe`,
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
+              wrapperStyle: `margin-bottom: 1.0725rem`
+            }
           },
           `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-        ],
-      },
+          `gatsby-remark-smartypants`
+        ]
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -99,16 +93,16 @@ module.exports = {
         short_name: `Alex Strand`,
         start_url: `/`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
-      },
+        icon: `content/assets/gatsby-icon.png`
+      }
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-  ],
-}
+        pathToConfigModule: `src/utils/typography`
+      }
+    }
+  ]
+};
