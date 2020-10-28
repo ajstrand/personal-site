ffa9ae49ddeca6bd12275609dbf980b470fa547c
Alex <ajstrand8@gmail.com>
2020-10-27 23:11:59 -0400

    add tmp directory to gitignore

--- new-toast-site/.tmp/src/pages/styles.js
+++
@@ -1,5 +0,0 @@
-/** @jsx jsx */
-import { jsx } from "@emotion/core";
-import StyleGuideComponent from "../temp/StyleGuideComponent.js";
-const StylesDemo = () => jsx(StyleGuideComponent, null);
-export default StylesDemo;
