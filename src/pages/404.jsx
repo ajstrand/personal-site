51c9c8f63f99cb84bf71f653698971a2ff550a3d
Alex Strand <ajstrand8@gmail.com>
2023-03-18 00:52:28 -0400

    fix some css. need to try and remove some css as there is starting rto be a lot of duplication

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -5,7 +5,7 @@ export const meta = {
 export default function NotFoundPage({ meta, pages }) {
   return (
     <main class="w-screen h-screen flex flex-col justify-center items-center">
-      <h3 class="font-bold text-blue dark:text-lightBlue">Not Found</h3>
+      <h3 class="not-found font-bold text-blue dark:text-lightBlue">Not Found</h3>
       <p>You've found a page that no longer exists</p>
     </main>
   );
